905e654a6a2629257c73a7c5c543c620e2eb9a9d
Xing Han Lu <xhlperso@gmail.com>
2018-12-30 15:49:15 -0500

    Drafted out all the sections

--- tutorial/dash_cytoscape_index.py
+++ tutorial/dash_cytoscape_index.py
@@ -35,8 +35,8 @@ layout = html.Div([
     > extends and renders [Cytoscape.js](http://js.cytoscape.org), and 
     > offers deep integration with Dash layouts and callbacks, enabling the 
     > creation of powerful networks jointly with the rich collection of Dash 
-    > components, as well as powerful computational biology and network science
-    > libraries such as Biopython and networkX.
+    > components, as well as established computational biology and network 
+    > science libraries such as Biopython and networkX.
     >
     > -- xhlulu
     ''')),
@@ -63,7 +63,8 @@ layout = html.Div([
             '/cytoscape/elements',
             '''
             Elements declaration in Cytoscape are designed to be clear, simple,
-            and JSON-friendly. Here's how it works:
+            and JSON-friendly. This chapter will get you started 
+            with examples for:
             - A
             - B
             - C
@@ -71,10 +72,78 @@ layout = html.Div([
         ),
 
         Chapter(
-            'Part X. Object Reference',
+            'Part 2. Layout',
+            '/cytoscape/layout',
+            '''
+            Make your graphs interpretable by using the built-in 
+            collection of easy-to-modify layouts. We show you how to:
+            - Display pre-determined and random layouts
+            - Represent your graph as a circle, a grid or a tree
+            - Finetune your representations by modifying the default options
+            - Use physics-based simulations to generate your layout 
+            '''
+        ),
+
+        Chapter(
+            'Part 3. Styling',
+            '/cytoscape/styling',
+            '''
+            Modify the color, shape and style of your elements with a syntax
+            similar to CSS. Cytoscape incldues a wide variety of properties,
+            equiping you with everything you need to display your graphs with
+            aesthetics, creativity, and understandability in mind. This chapter
+            covers:
+            - Basic style properties for nodes and edges
+            - Using selectors to modify specific groups of elements
+            - Organize your edges with curve and line properties
+            - Advanced node styling with pie charts and images
+            '''
+        ),
+
+        Chapter(
+            'Part 4. Dash Callbacks',
+            '/cytoscape/callbacks',
+            '''
+            Update your layout, elements, or style with other Dash components
+            using callbacks. This chapter covers:
+            - Adding and removing elements
+            - Modifying the layout and the stylesheet
+            - Advanced usage of callbacks
+            '''
+        ),
+
+        Chapter(
+            'Part 5. Events Callbacks for User Interactions',
+            '/cytoscape/events',
+            '''
+            Dash Cytoscape fires callbacks whenever the user interact with the
+            graph you created, which can be used to update the graph itself,
+            or to interact with other components. This chapter includes 
+            examples for:
+            - Simple integrations with HTML components
+            - Different types of data returned
+            - Node versus edges callbacks
+            = Tap, hover or select callbacks
+            '''
+        ),
+
+        Chapter(
+            'Part 6. Applications',
+            '/cytoscape/applications',
+            '''
+            We show how Dash Cytoscape can be applied for fields as diverse as
+            bioinformatics and social networks analysis. Examples include:
+            - Displaying Phylogeny Data with Biopython
+            - Interactive exploration of large social networks
+            '''
+        ),
+
+
+        Chapter(
+            'Part 7. Object Reference',
             '/cytoscape/reference',
             '''
-            The complete list of properties of the Cytoscape object.
+            You can find here all the properties of the `Cytoscape` object.
             '''
         )
     ]),
